403fdb6d5e092186dbe13391bbb0cf9478e60413
unknown <dustinmiller032@gmail.com>
2016-03-30 11:25:02 -0500

    Deleted a needed variable

--- utils/slack.js
+++ utils/slack.js
@@ -3,6 +3,8 @@
 const request = require('request');
 const camelCase = require('camelcase-keys-recursive');
 
+var internals = {};
+
 module.exports = internals.Slack = function(options) {
   this.token = options.token;
 };
