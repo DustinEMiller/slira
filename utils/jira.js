57931efcdbd21984b489a1ae8e1580bcb1d0c995
unknown <dustinmiller032@gmail.com>
2016-03-30 12:09:54 -0500

    Adding error catching

--- utils/jira.js
+++ utils/jira.js
@@ -23,6 +23,9 @@ module.exports.queryIssues = function() {
 		if (!error && response.statusCode == 200) {
     		var info = JSON.parse(body);
     		console.log(info);
+  		} else {
+  			console.log(error);
+  			console.log(response);	
   		}
 	});
 }
