385e1fe167f669d07aeff8aa945bad7cc59c43cd
unknown <dustinmiller032@gmail.com>
2016-03-30 16:39:17 -0500

    JSONify reply in queryIssue

--- controllers/jira.js
+++ controllers/jira.js
@@ -38,7 +38,7 @@ module.exports.slackHook = function(request, reply) {
           'color': '#F35A00'
         }
       });
-      reply(message);
+      reply(JSON.stringify(message));
     })
     .catch((err) => {
       reply(Boom.badImplementation(err));
