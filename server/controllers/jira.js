bd3eb9eba3587b9f31f320eb7f5bc3dd9497d6cb
unknown <dustinmiller032@gmail.com>
2016-08-03 16:07:37 -0500

    commit to upload to get around company self signed cert, cap issue

--- server/controllers/jira.js
+++ server/controllers/jira.js
@@ -41,11 +41,7 @@ module.exports.slackHook = function(request, reply) {
     } else if (command[0] === 'comment' || command[0] === 'c') {
         mechanism = JIRA.addComment(argString);
     } else if (command[0] === 'connect') {
-        JIRA.createConnectionLink(request.payload).
-            then((result) => {
-                console.log(result);
-            });
-
+        console.log(JIRA.createConnectionLink(request.payload));
     } else if (command[0] === 'help') {
         return reply(JIRA.help(1)).header('content-type', 'application/json');
     } else {
