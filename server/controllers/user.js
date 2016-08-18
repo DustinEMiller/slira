f85a1441e396597efa9e4dd4af6e98787885fa04
unknown <dustinmiller032@gmail.com>
2016-08-18 10:36:32 -0500

    authentication redoing because grunt tasks take forever to run on this machine

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,7 +24,7 @@ module.exports.addNew = (request, reply) => {
 
 	User.findOne({ email: user.email}).exec()
 		.then((response) => {
-			if(repsonse.email === user.email) {
+			if(response.email === user.email) {
 				return reply({success: false , msg: 'There is already an account associated with that email address.'});	
 			}
 			
