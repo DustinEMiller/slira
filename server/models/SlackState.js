c68516345eb5f7cf05970e5361f16594ded39d3c
unknown <dustinmiller032@gmail.com>
2016-08-24 15:34:12 -0500

    redoing login and registration flow with slack oauth

--- server/models/SlackState.js
+++ server/models/SlackState.js
@@ -21,18 +21,20 @@ let mongoose = require('mongoose'),
     });
 
 SlackStateSchema.pre('save', function(next) {
-	this.created_at = new Date();
-	this.delete_at = new Date(this.created_at);
-	this.delete_at.setTime(this.created_at.getTime() + 1440*60000);
+    let slackState = this;
+	slackState.created_at = new Date();
+	slackState.delete_at = new Date(slackState.created_at);
+	slackState.delete_at.setTime(slackState.created_at.getTime() + 1440*60000);
+
+    console.log(this);
 
     crypto.randomBytes(48, function(err, buffer) {
         if(err) {
             return next(err);
         }
-
-        this.state = buffer.toString('hex');
-        console.log(this.state);
-        return next();
+        console.log(this);
+        slackState.state = buffer.toString('hex');
+        next();
     });
 });
 
