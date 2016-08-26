cda1fcb5702ad95923a6f979f5623db5f5f86f78
unknown <dustinmiller032@gmail.com>
2016-08-26 14:53:06 -0500

    stuff

--- server/routes.js
+++ server/routes.js
@@ -53,9 +53,6 @@ module.exports = [
     	path: '/login/slack',
 		config: {
     		auth: 'slack',
-    		pre: [{method: function(thing, afterThing) {
-    			console.log(thing);
-    		}}],
     		handler: function(request, reply){
     			console.log(request.auth.credentials.profile);
       			reply.file('index.html');
