06944b69e7aa76aa41ac4700ad12c4264f3eb398
unknown <dustinmiller032@gmail.com>
2016-09-22 10:17:55 -0500

    testing out scopes

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ server.register([require('hapi-auth-jwt'), require('hapi-auth-cookie'), require(
         password: 'dlksjdgjinrimirmnginhcoihgirhjgijcobpsdkgowkr',
         clientId: config.slack.clientId,
         clientSecret: config.slack.oauthSecret,
-        scope: ['identity.basic', 'identity.email'],
+        scope: ['identify'],
         isSecure: false     // Terrible idea but required if not using HTTPS especially if developing locally
     });
 
