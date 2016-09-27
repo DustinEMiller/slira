951443ae3c57f51530b867555dc8c774976e6dda
DustinEMiller <dustinmiller032@gmail.com>
2016-09-27 15:46:41 -0500

    duplicate handler name

--- server/controllers/jira.js
+++ server/controllers/jira.js
@@ -60,7 +60,7 @@ module.exports.slackHook = function(request, reply) {
 
 module.exports.checkUser = function(request, reply) {
     return reply(JIRA.checkUser().header('content-type', 'application/json'));
-}
+};
 
 module.exports.issueUpdatedHook = function(request, reply) {
     if (request.payload.hasOwnProperty("comment")) {
